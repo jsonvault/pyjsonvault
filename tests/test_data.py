80daf9972e516cf2a41556b5bd6c64143caa65ac
Mario Salazar de Torres <mario.sb93@gmail.com>
2020-04-12 18:40:12 +0200

    Add entry equality operator
     * Added operator.
     * Added tests for the operator.
    
    Issue: #18
    Labels: development testing

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,3 +1,5 @@
+import pytest
+
 from jsonvault.data import Entry
 
 
@@ -13,3 +15,44 @@ def test_create_entry():
     assert entry is not None
     assert entry.key == key
     assert entry.value == value
+
+
+def test_entry_equality_compare():
+    """
+    Scenario. Two equal entries are compared.
+    Expected result. Comparison should return True.
+    """
+
+    key, value = "key", "value"
+    entry = Entry(key, value)
+    other_entry = Entry(key, value)
+
+    assert entry == other_entry
+
+
+def test_entry_equality_compare_different_type():
+    """
+    Scenario. Two equal entries are compared.
+    Expected result. Comparison should return True.
+    """
+
+    key, value = "key", "value"
+    entry = Entry(key, value)
+
+    assert entry != value
+
+
+@pytest.mark.parametrize("first_entry_tuple, second_entry_tuple", [
+    (("key", "value"), ("key", "value_different")),
+    (("key", "value"), ("key_different", "value")),
+    (("key", "value"), ("key_different", "value_different"))
+])
+def test_entry_equality_compare_different_content(first_entry_tuple, second_entry_tuple):
+    """
+    Scenario. Two equal entries are compared.
+    Expected result. Comparison should return True.
+    """
+
+    first_entry = Entry(*first_entry_tuple)
+    second_entry = Entry(*second_entry_tuple)
+    assert first_entry != second_entry
